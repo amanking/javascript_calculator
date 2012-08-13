fef81bf82572c38f521e5c31eac16e95b80a11ce
Aman King <amanking@gmail.com>
2012-08-13 23:35:00 +0530

    Initial setup

---
+++ spec/calculator.spec.js
@@ -0,0 +1,10 @@
+describe("Calculator", function() {
+
+    describe("Entering single digits", function() {
+
+        it("should display 0 when entering 0", function() {
+            expect(true).toEqual(true);
+        });
+
+    });
+});
